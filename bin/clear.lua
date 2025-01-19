72b5a513c8fec464571f42938d7c30f674387fc3
Vladimir Berezin <72685561+vberezinbadger@users.noreply.github.com>
2025-01-20 00:30:26 +0300

    Delete bin directory
    Signed-off-by: Vladimir Berezin <72685561+vberezinbadger@users.noreply.github.com>

--- bin/clear.lua
+++
@@ -1,5 +0,0 @@
--- clear: clear the screen --
-
-local w,h = gpu.getResolution()
-gpu.fill(1,1,w,h," ")
-gpu.setCursor(1,1)
--- bin/uptime.lua
+++
@@ -1,5 +0,0 @@
--- uptime: system uptime --
-
-local computer = require("computer")
-
-print("Computer working time: " .. tostring(math.floor(computer.uptime())) .. "s")
